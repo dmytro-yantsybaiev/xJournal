1ea2fec8067269d51c094491aeeaaadfb7114e26
dmytro-yantsybaiev <d.yantsybaiev@gmail.com>
2025-08-18 20:03:25 +0200

    Initial Commit

---
+++ xJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xJournal
+//
+//  Created by Dmytro Yantsybaiev on 18.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ xJournalTests/xJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  xJournalTests.swift
+//  xJournalTests
+//
+//  Created by Dmytro Yantsybaiev on 18.08.2025.
+//
+
+import Testing
+@testable import xJournal
+
+struct xJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
